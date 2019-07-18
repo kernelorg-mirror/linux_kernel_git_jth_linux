4ac3332af1fa5ba7ecb92181329151382b800f3d
Johannes Thumshirn <jthumshirn@suse.de>
2019-07-18 10:12:47 +0200

    btrfs: use xxhash64 for checksumming
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- fs/btrfs/ctree.h
+++ fs/btrfs/ctree.h
@@ -90,6 +90,7 @@ static const struct btrfs_csums {
 	const char	*name;
 } btrfs_csums[] = {
 	BTRFS_CHECKSUM_TYPE(BTRFS_CSUM_TYPE_CRC32, 4, "crc32c"),
+	BTRFS_CHECKSUM_TYPE(BTRFS_CSUM_TYPE_XXHASH, 8, "xxhash64"),
 };
 
 #define BTRFS_EMPTY_DIR_SIZE 0
