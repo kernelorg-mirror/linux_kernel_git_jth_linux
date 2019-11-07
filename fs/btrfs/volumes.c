c0242d0b33606f4ae84fff37b57682e5b8b6995e
Johannes Thumshirn <jthumshirn@suse.de>
2019-11-07 15:16:57 +0100

    btrfs: decrement number of open devices after closing the device not before
    In btrfs_close_one_device we're decrementing the number of open devices
    before we're calling btrfs_close_bdev().
    
    As there is no intermediate exit between these points in this function it
    is technically OK to do so, but it a) is a bit harder to understand and b)
    further refactoring to implement proper error handling in
    btrfs_close_one_device() will change the layout of the function.
    
    Move both operations closer together and move the decrement step after
    btrfs_close_bdev().
    
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -1067,9 +1067,6 @@ static void btrfs_close_one_device(struct btrfs_device *device)
 	struct btrfs_device *new_device;
 	struct rcu_string *name;
 
-	if (device->bdev)
-		fs_devices->open_devices--;
-
 	if (test_bit(BTRFS_DEV_STATE_WRITEABLE, &device->dev_state) &&
 	    device->devid != BTRFS_DEV_REPLACE_DEVID) {
 		list_del_init(&device->dev_alloc_list);
@@ -1080,6 +1077,8 @@ static void btrfs_close_one_device(struct btrfs_device *device)
 		fs_devices->missing_devices--;
 
 	btrfs_close_bdev(device);
+	if (device->bdev)
+		fs_devices->open_devices--;
 
 	new_device = btrfs_alloc_device(NULL, &device->devid,
 					device->uuid);
